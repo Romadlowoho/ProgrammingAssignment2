159e90d10b35f764c8c9e53b84e138487187ccb9
Romadlowoho <roma.ushpayev@gmail.com>
2016-07-09 17:05:09 -0700

    Caching the inverse of a matrix
    The following two functions utilize lexical scoping to cache the inverse
    of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,35 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## So in this assignmment I am going to utilize a special property of R 
+## called Lexical Scoping
+## The goal is to cache the inverse of a matrix so that it doesn't 
+## have to compute it again
 
-## Write a short comment describing this function
-
+## The following function computes the inverse of a matrix  and caches
+## the inverse of it
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
+    ## The next function is where Lexical Scoping comes into play
     set <- function(y) {
       x <<- y
       inv <<- NULL
     }
+    ## Calculating the inverse
     get <- function() x
-    setInverse <- function(inverse) inv <<- inverse
-    getInverse <- function() inv
-    list(set = set,
-         get = get,
-         setInverse = setInverse,
+    setTheInverse <- function(inverse) inv <<- inverse
+    getTheInverse <- function() inv
+    list(set = set, get = get, setInverse = setInverse,
          getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## The following function too computes the inverse of a matrix, but first 
+## checks if it has already been computed.
+## If so, it retrieves it from the cache
+## If not, calculates it
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   inv <- x$getInverse()
-  if (!is.null(inv)) {
+  inverse <- x$getTheInverse()
     message("getting cached data")
-    return(inv)
+    return(inverse)
   }
+  ## If it is not in the cache, calculates it
   mat <- x$get()
-  inv <- solve(mat, ...)
-  x$setInverse(inv)
-  inv
+  inverse <- solve(mat, ...)
+  x$setTheInverse(inverse)
+  inverse
 }
